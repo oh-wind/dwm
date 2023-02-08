03156cf6807575a1fb890f0ffe6c0809e421a966
cybird <plutohu@outlook.com>
2023-02-09 04:52:41 +0800

    apply patch: functionalgaps-6.2

--- config.h
+++ config.h
@@ -7,6 +7,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int startwithgaps	     = 1;	 /* 1 means gaps are used by default */
+static const unsigned int gappx     = 4;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
